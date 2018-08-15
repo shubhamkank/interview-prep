99366af68f246bc8e1fc074c412d04e3e3cfd3cb
Shubham Kankaria <shubhamkank@gmail.com>
2018-08-15 18:57:13 +0530

    Bit manipulation contd

--- src/main/java/com/interview/leetcode/BitManipulation.java
+++ src/main/java/com/interview/leetcode/BitManipulation.java
@@ -19,10 +19,77 @@ public class BitManipulation {
         return getSum(sum, carry);
     }
 
+    public static int getSubtract(int a, int b) {
+        if(b == 0)
+            return a;
+        int sum = a ^ b;
+        int borrow = (~a & b) << 1;
+        return getSum(sum, borrow);
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) == n;
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+
+    public static int missingNumber(int [] num) {
+        int result = 0;
+        for(int i = 0; i < num.length; i++) {
+            result ^= i;
+            result ^= num[i];
+        }
+        return result ^ num.length;
+    }
+
+    public static int reverseBits(int n) {
+        int rev = 0;
+        while(n > 0) {
+            rev <<= 1;
+
+            if((int)(n & 1) == 1) {
+                rev ^= 1;
+            }
+
+            n >>= 1;
+        }
+        return rev;
+    }
+
+    public static int largestPower(int n) {
+        n = n | (n >> 1);
+        n = n | (n >> 2);
+        n = n | (n >> 4);
+        n = n | (n >> 8);
+        n = n | (n >> 12);
+
+        return (n+1) >> 1;
+    }
+
+    public static int bitwiseRangeAnd(int m, int n) {
+        int a = 0;
+        while(m != n) {
+            m >>= 1;
+            n >>= 1;
+            a++;
+        }
+        return m << a;
+    }
+
     public static void main(String[] args) {
-        System.out.println(getSum(1, 2));
-        System.out.println(getSum(5, 5));
-        System.out.println(getSum(3, 3));
-        System.out.println(getSum(-1, 3));
+        System.out.println(largestPower(3));
+        System.out.println(largestPower(1));
+        System.out.println(largestPower(15));
+        System.out.println(largestPower(255));
+        System.out.println(largestPower(-3));
+
+        System.out.println(reverseBits(11));
+
+        System.out.println(bitwiseRangeAnd(5, 7));
+        System.out.println(bitwiseRangeAnd(7, 15));
+
+        System.out.println(getSubtract(7, 4));
     }
 }
