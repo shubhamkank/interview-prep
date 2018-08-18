d2bf1b2b92058419f7ac8b7c63e34c3ed3869fc5
Shubham Kankaria <shubhamkank@gmail.com>
2018-08-18 22:38:57 +0530

    Maximum Subarray

---
+++ src/main/java/com/interview/leetcode/MaxSubArray.java
@@ -0,0 +1,42 @@
+package com.interview.leetcode;
+
+public class MaxSubArray {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            maxSum = Math.max(sum, maxSum);
+            for(int j = i+1; j < nums.length; j++) {
+                sum += nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+
+    /**
+     * Kadane's Algorithm: maxSum[i] = max(A[i], A[i] + maxSum[i-1])
+     * Time Complexity: O(n) Space Complexity: O(1)
+     */
+    public static int maxSubArray2(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int maxSum = nums[0];
+        int maxGlobalSum = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            maxSum = Math.max(nums[i], maxSum + nums[i]);
+            maxGlobalSum = Math.max(maxGlobalSum, maxSum);
+        }
+        return maxGlobalSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray2(new int [] {-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSubArray2(new int [] {1, 2, 3, 4, 5}));
+        System.out.println(maxSubArray2(new int [] {1, -2, -3, -4, -5}));
+    }
+}
