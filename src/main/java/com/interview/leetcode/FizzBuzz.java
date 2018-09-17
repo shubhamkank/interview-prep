9a9fb13120695a75f9974362a8bcaef2bce838a5
Shubham Kankaria <shubhamkank@gmail.com>
2018-09-18 01:16:50 +0530

    Fizz Buzz

---
+++ src/main/java/com/interview/leetcode/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.interview.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                res.add("Fizz");
+            } else if(i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(Integer.toString(i));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(15).forEach(System.out::println);
+    }
+}
