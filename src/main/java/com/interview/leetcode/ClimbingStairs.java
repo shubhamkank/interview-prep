2b4508dd802c80fcdcce11b07783a587e75e07c3
Shubham Kankaria <shubhamkank@gmail.com>
2018-09-18 01:01:28 +0530

    Climbing Stairs

--- src/main/java/com/interview/leetcode/ClimbingStairs.java
+++ src/main/java/com/interview/leetcode/ClimbingStairs.java
@@ -66,9 +66,50 @@ public class ClimbingStairs {
         return second;
     }
 
+    // Fibonacci Number - Binets Method
+    // Time complexity: O(logn), Space complexity: O(1)
+    // M^n = Product(M^(2^p)) where value of p is the position of set bits in n
+    // For example, M^7 = M^2^0 * M^2^1 * M^2^2
+    public static int climbStairs5(int n) {
+        int [][] q = {{1, 1}, {1, 0}};
+        int [][] result = pow(q, n);
+        return result[0][0];
+    }
+
+    private static int[][] pow(int[][] a, int n) {
+        int [][] ret = {{1, 0}, {0, 1}};
+
+        while (n > 0) {
+            if((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    private static int[][] multiply(int[][] a, int[][] b) {
+        int [][] c = new int[2][2];
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
+
+    // Fibonacci Number - Formula
+    // Time complexity: O(logn), Space complexity: O(1) - Math.pow takes logn time
+    public static int climbStairs6(int n) {
+        double sqrt5 = Math.sqrt(5);
+        double fibn = Math.pow((1 + sqrt5)/2, n + 1) - Math.pow((1 - sqrt5)/2, n + 1);
+        return (int)(fibn/sqrt5);
+    }
+
     public static void main(String [] args) {
-        System.out.println(climbStairs4(3));
-        System.out.println(climbStairs4(4));
-        System.out.println(climbStairs4(5));
+        System.out.println(climbStairs6(3));
+        System.out.println(climbStairs6(4));
+        System.out.println(climbStairs6(5));
     }
 }
