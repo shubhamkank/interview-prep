efd0c8d8e627da8626375cb57ea685cbb8de2f1c
Shubham Kankaria <shubhamkank@gmail.com>
2019-04-04 23:35:12 +0530

    Most stones removed - dfs

--- src/main/java/com/interview/leetcode/MostStonesRemoved.java
+++ src/main/java/com/interview/leetcode/MostStonesRemoved.java
@@ -5,13 +5,16 @@ import java.util.Map;
 
 public class MostStonesRemoved {
 
+    /* Union-find approach
+       Use indexes instead of considering points
+       Time complexity: O(n), Space complexity: O(n) */
     public static int removeStones(int[][] stones) {
         if(stones == null || stones.length < 2) {
             return 0;
         }
         UnionFind uf = new UnionFind();
         for(int i = 0; i < stones.length; i++) {
-            uf.union(stones[i][0], ~stones[i][1]);
+            uf.union(stones[i][0], ~stones[i][1]);  // ~i = -i - 1
         }
         return stones.length - uf.getCount();
     }
@@ -63,8 +66,39 @@ public class MostStonesRemoved {
         }
     }
 
+    /* Depth-first search
+       Time complexity: O(n), Space complexity: O(n) */
+    public static int removeStones2(int[][] stones) {
+        if(stones == null || stones.length < 2) {
+            return 0;
+        }
+
+        boolean[] visited = new boolean[stones.length];
+        int numOfIslands = 0;
+
+        for (int i = 0; i < stones.length; i++) {
+            if(!visited[i]) {
+                dfsUtil(stones, i, visited);
+                numOfIslands++;
+            }
+        }
+        return stones.length - numOfIslands;
+    }
+
+    private static void dfsUtil(int[][] stones, int idx, boolean[] visited) {
+        visited[idx] = true;
+        for (int i = 0; i < stones.length; i++) {
+            if(visited[i]) {
+                continue;
+            }
+            if(stones[i][0] == stones[idx][0] || stones[i][1] == stones[idx][1]) {
+                dfsUtil(stones, i, visited);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[][] stones = {{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}};
-        System.out.println(removeStones(stones));
+        System.out.println(removeStones2(stones));
     }
 }
