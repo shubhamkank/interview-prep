ad985e71ba724d8f705b2d962ad0e1b59b76c0f3
Shubham Kankaria <shubhamkank@gmail.com>
2019-04-21 01:16:35 +0530

    Network Delay Time - Dijkstra

--- src/main/java/com/interview/leetcode/NetworkDelayTime.java
+++ src/main/java/com/interview/leetcode/NetworkDelayTime.java
@@ -1,6 +1,6 @@
 package com.interview.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class NetworkDelayTime {
 
@@ -28,7 +28,54 @@ public class NetworkDelayTime {
         return result == Integer.MAX_VALUE ? -1 : result;
     }
 
+    /* Dijkstra's Algorithm
+       Time complexity: O((V+E)logV) = O(ElogV), Space complexity: O(V) */
+    public static int networkDelayTime2(int[][] times, int N, int K) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for(int[] time : times) {
+            graph.putIfAbsent(time[0], new ArrayList<>());
+            graph.get(time[0]).add(new int[] {time[1], time[2]});
+        }
+
+        int[] distTo = new int[N + 1];
+        Arrays.fill(distTo, Integer.MAX_VALUE);
+        distTo[K] = 0;
+
+        boolean[] visited = new boolean[N + 1];
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.add(new int[]{K, 0});
+
+        int result = 0;
+
+        while(!pq.isEmpty()) {
+            int[] cur = pq.remove();
+            int curNode = cur[0];
+            if(visited[curNode]) {
+                continue;
+            }
+            visited[curNode] = true;
+
+            int curDist = cur[1];
+            result = curDist;
+            N--;
+
+            if(graph.containsKey(curNode)) {
+                for(int[] next : graph.get(curNode)) {
+                    int nextNode = next[0];
+                    int nextDist = next[1];
+
+                    if(!visited[nextNode] && distTo[nextNode] > curDist + nextDist) {
+                        distTo[nextNode] = curDist + nextDist;
+                        pq.add(new int[] {nextNode, distTo[nextNode]});
+                    }
+                }
+            }
+        }
+        return N == 0 ? result : -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(networkDelayTime(new int[][]{{2,1,1}, {2,3,1}, {3,4,1}}, 4, 2));
+        System.out.println(networkDelayTime2(new int[][]{{2,1,1}, {2,3,1}, {3,4,1}}, 4, 2));
     }
 }
