0256a7551646584079e6861f756584973e1aea73
Shubham Kankaria <shubhamkank@gmail.com>
2019-10-08 00:14:17 +0530

    Find and replace in string

---
+++ src/main/java/com/interview/leetcode/FindAndReplaceInString.java
@@ -0,0 +1,36 @@
+package com.interview.leetcode;
+
+import java.util.Arrays;
+
+public class FindAndReplaceInString {
+
+    public static String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
+        int N = S.length();
+        int[] match = new int[N];
+        Arrays.fill(match, -1);
+
+        for (int i = 0; i < indexes.length; i++) {
+            int idx = indexes[i];
+            if(S.substring(idx, idx + sources[i].length()).equals(sources[i])) {
+                match[idx] = i;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < N) {
+            if(match[i] >= 0) {
+                sb.append(targets[match[i]]);
+                i += sources[match[i]].length();
+            } else {
+                sb.append(S.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findReplaceString("abcd", new int[] {0, 2}, new String[] {"a", "cd"}, new String[]{"eee", "ffff"}));
+    }
+}
